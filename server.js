fadc8c81f13522c286ca58d1a575afca48cb8a4f
Julien Az <fromscratch.frontdev@gmail.com>
2020-11-10 12:40:26 +0100

    test

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 // const path = require("path");
 // app.use(express.static(path.join(__dirname, "./public/")));
 
